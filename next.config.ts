ba556605f6793ee0a57a947c3b332d059a636e58
WIT <wit.chanthawat@gmail.com>
2025-07-14 06:40:44 +0700

    fix: update next.config.ts to include additional image domains and enable unoptimized loading
    feat: add custom image loader in image-loader.js

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,13 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
     domains: [
-      "raw.githubusercontent.com",       // for GitHub-hosted token logos
+      "raw.githubusercontent.com", // for GitHub-hosted token logos
       "tomato-rear-quokka-6.mypinata.cloud", // for your Pinata-hosted assets
-      "arweave.net",                     // common for Solana logos
-      "shdw.drive.genesysgo.net"        // optional: for Shadow Drive if used
+      "arweave.net", // common for Solana logos
+      "shdw.drive.genesysgo.net", // optional: for Shadow Drive if used
     ],
+    unoptimized: true,
+    formats: [],
   },
 };
 
